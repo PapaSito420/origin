d501ccca2cefb220adc29055e4ed7a0df5f66362
Andrey Menshikov <andreymenshikov420@icloud.com>
2023-11-11 17:47:17 +0300

    Task_7_1 update

---
+++ Task_7_1/Task_7_1/math.hpp
@@ -0,0 +1,18 @@
+//
+//  math.hpp
+//  Task_7_1
+//
+//  Created by Andrey Menshikov on 08.11.2023.
+//
+
+#ifndef math_hpp
+#define math_hpp
+
+#include <stdio.h>
+int add(int x, int y);
+int mns(int x, int y);
+int mult (int x, int y);
+double del (int x, int y);
+int degree (int x, int y);
+
+#endif /* math_hpp */
