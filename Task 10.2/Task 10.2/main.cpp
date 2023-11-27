fae316fe1796eb17ae16122cf61faa89386d04d5
Andrey Menshikov <andreymenshikov420@icloud.com>
2023-11-27 12:37:52 +0300

    Task 10.2

--- Task 10.2/Task 10.2/main.cpp
+++ Task 10.2/Task 10.2/main.cpp
@@ -37,64 +37,91 @@
 
 #include <iostream>
 
-class Fraction
-{
+class Fraction {
 private:
-int numerator_;
-int denominator_;
+    int numerator_;
+    int denominator_;
 
 public:
-    Fraction(int numerator, int denominator): numerator_(numerator), denominator_(denominator){}
-    
-    
-       bool operator==(const Fraction& other) const {
-           return numerator_ * other.denominator_ == denominator_ * other.numerator_;
-       }
+    Fraction(int numerator, int denominator) : numerator_(numerator), denominator_(denominator) {}
 
-       
-       bool operator!=(const Fraction& other) const {
-           return !(*this == other);
-       }
+    Fraction operator+(const Fraction& other) const {
+        return Fraction(numerator_ * other.denominator_ + other.numerator_ * denominator_,
+                        denominator_ * other.denominator_);
+    }
 
-       
-       bool operator<(const Fraction& other) const {
-           return numerator_ * other.denominator_ < denominator_ * other.numerator_;
-       }
+    Fraction operator-(const Fraction& other) const {
+        return Fraction(numerator_ * other.denominator_ - other.numerator_ * denominator_,
+                        denominator_ * other.denominator_);
+    }
 
-       
-       bool operator>(const Fraction& other) const {
-           return other < *this;
-       }
+    Fraction operator*(const Fraction& other) const {
+        return Fraction(numerator_ * other.numerator_, denominator_ * other.denominator_);
+    }
 
-       
-       bool operator<=(const Fraction& other) const {
-           return !(*this > other);
-       }
+    Fraction operator/(const Fraction& other) const {
+        return Fraction(numerator_ * other.denominator_, denominator_ * other.numerator_);
+    }
 
-       
-       bool operator>=(const Fraction& other) const {
-           return !(*this < other);
-       }
+    Fraction operator-() const {
+        return Fraction(-numerator_, denominator_);
+    }
 
-    friend std::istream& operator >> (  std::istream& in , Fraction &other ){
-        char slash;
-        in >> other.denominator_ >> slash >> other.numerator_;
-        return in;
+    Fraction& operator++() {
+        numerator_ += denominator_;
+        return *this;
+    }
+
+    Fraction operator++(int) {
+        Fraction temp = *this;
+        ++(*this);
+        return temp;
+    }
+
+    Fraction& operator--() {
+        numerator_ -= denominator_;
+        return *this;
+    }
+
+    Fraction operator--(int) {
+        Fraction temp = *this;
+        --(*this);
+        return temp;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Fraction& fraction) {
+        os << fraction.numerator_ << "/" << fraction.denominator_;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Fraction& fraction) {
+        char slash; // Для пропуска символа '/'
+        is >> fraction.numerator_ >> slash >> fraction.denominator_;
+        return is;
     }
 };
 
+int main() {
+    Fraction f1(1, 1);
+    Fraction f2(1, 1);
 
+    std::cout << "Введите числитель и знаменатель дроби 1 в формате a/b: ";
+    std::cin >> f1;
+    std::cout << "Введите числитель и знаменатель дроби 2 в формате a/b: ";
+    std::cin >> f2;
 
-int main()
-{
-Fraction f1(4, 3);
-Fraction f2(6, 11);
+    std::cout << f1 << " + " << f2 << " = " << (f1 + f2) << "\n";
+    std::cout << f1 << " - " << f2 << " = " << (f1 - f2) << "\n";
+    std::cout << f1 << " * " << f2 << " = " << (f1 * f2) << "\n";
+    std::cout << f1 << " / " << f2 << " = " << (f1 / f2) << "\n";
 
-std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
-std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
-std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
-std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
-std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
-std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
-return 0;
+    Fraction preIncrementedF1 = ++f1;
+    std::cout << "++" << f1 << " * " << f2 << " = " << (preIncrementedF1 * f2) << "\n";
+    std::cout << "Значение дроби 1 = " << f1 << "\n";
+
+    Fraction postDecrementedF1 = f1--;
+    std::cout << f1 << "-- * " << f2 << " = " << (postDecrementedF1 * f2) << "\n";
+    std::cout << "Значение дроби 1 = " << f1 << "\n";
+    
+    return 0;
 }
